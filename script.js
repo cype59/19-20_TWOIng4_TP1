0264bdd1e5e1263517f94af1e68dd49e1f363a98
Cyril Pierron <cype59@gmail.com>
2019-10-15 09:33:46 +0200

    ajout script pour le scroll

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click, code trouvé sur https://wprock.fr/blog/javascript-jquery-smooth-scrolling/
+    **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1000);
+            }
+        }
+    });
+});
